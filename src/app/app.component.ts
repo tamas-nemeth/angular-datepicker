754ab7c8ed855c6174e33b0e122add65467265b7
Tamas Nemeth <tamas.nemeth@edreamsodigeo.com>
2019-01-15 15:45:27 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nghu-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'angular-datepicker';
+}
